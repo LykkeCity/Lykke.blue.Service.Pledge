ed8bbe737a57f3220adcf8821419cbe45945188c
Miladin Joksic <miladin.joksic@primeholding.com>
2017-10-23 15:47:22 +0200

    Minor code refactoring

--- src/Lykke.Service.Pledges/Requests/CreatePledgeRequest.cs
+++ src/Lykke.Service.Pledges/Requests/CreatePledgeRequest.cs
@@ -1,12 +1,14 @@
 ﻿using Lykke.Service.Pledges.Core.Domain;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace Lykke.Service.Pledges.Requests
 {
     public class CreatePledgeRequest : IPledge
     {
-        [Required]
-        public string Id { get; set; }
+        //REMARK: We do not need to allow someone to set Id. Id is set automatically.
+        [IgnoreDataMember]
+        public string Id { get; }
         [Required]
         public string ClientId { get; set; }
         [Required]
